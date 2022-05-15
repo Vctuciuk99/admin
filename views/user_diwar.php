d08af33efc3da4240f8ec1f0ae3032a0b31ac95d
Vctuciuk99 <rdrow589@gmail.com>
2022-05-15 18:03:51 +0800

    blah

--- views/user_diwar.php
+++ views/user_diwar.php
@@ -45,6 +45,15 @@
                 <?php if(isset($_GET['month'])){echo $_GET['month']; } ?>
             </select><br>
 
+
+            <!-- year -->
+            <label for="year">YEAR: </label>
+
+            <!-- PHP SA LOOB NG INPUT TAG TO GET VALUE USING PHP -->
+            <input type="text" name="year" 
+                <?php if(isset($_GET['year'])){echo $_GET['year']; } ?>
+                ><p>(Optional)</p>
+
             <!-- date -->
             <h6>search by date</h6>
             <label for="date">Date: </label>
@@ -85,24 +94,20 @@
                             $mysqli = require "../php/database_conn.php";
                             
                             //SEARCH RECORD WITH EMPLOYEE NO ONLY BY MONTHLY BASIS
-                            if(isset($_GET['month']) && empty($_GET['date']))
+                            if(isset($_GET['month']) && empty($_GET['date']) && empty($_GET['year']))
                             {
                                 $employee_no = $_GET['employee_no'];
                                 $month = $_GET['month'];
-                                //print_r($month);
-                                //$date = $_GET['date'];
-                                //print_r($date);
-                                //$query = "SELECT * FROM `user_diwar_record` WHERE `Employee_no` = '$employee_no' AND `Date` = '$date' ";
+                                //SQL COMMAND TO SORT DB
                                 $query = "SELECT * FROM `user_diwar_record` WHERE `Employee_no` = '$employee_no' AND DATE_FORMAT(Date,'%c')='$month' ";
                                 $query_run = mysqli_query($mysqli, $query);
-                                
+                                //IF RESULT IS > 0
                                 if(mysqli_num_rows($query_run) > 0)
                                 {
                                     foreach($query_run as $items)
                                     {
                                         ?>
                                         <tr>
-
                                             <!-- $items['COLUMN NAME IN USER_DIWAR DB'] -->
                                             <td><?= $items['Employee_no']; ?></td>
                                             <td><?= $items['Email']; ?></td>
@@ -126,27 +131,21 @@
                                     <?php
                                 }
                             }
-                            //search specific date only
-                            elseif ($_GET['month'] === 'Select Month' ) {
-                                
-                                # code...
+                            //SEARCH BY YEAR
+                            elseif (!empty($_GET['year'])) {
                                 $employee_no = $_GET['employee_no'];
-                                //$month = $_GET['month'];
-                                //print_r($month);
-
-                                $date = $_GET['date'];
-                                //print_r($date);
-                                $query = "SELECT * FROM `user_diwar_record` WHERE `Employee_no` = '$employee_no' AND `Date` = '$date' ";
-                                //$query = "SELECT * FROM `user_diwar_record` WHERE `Employee_no` = '$employee_no' AND DATE_FORMAT(Date,'%c')='$month' ";
+                                $year = $_GET['year'];
+                                $month = $_GET['month'];
+                                //SQL COMMAND TO SORT BY YEAR
+                                $query = "SELECT * FROM `user_diwar_record` WHERE `Employee_no` = '$employee_no' AND DATE_FORMAT(Date,'%c')='$month' AND DATE_FORMAT(Date,'%Y')='$year' ";
                                 $query_run = mysqli_query($mysqli, $query);
-                                
+                                //IF RESLUT IS > 0
                                 if(mysqli_num_rows($query_run) > 0)
                                 {
                                     foreach($query_run as $items)
                                     {
                                         ?>
                                         <tr>
-
                                             <!-- $items['COLUMN NAME IN USER_DIWAR DB'] -->
                                             <td><?= $items['Employee_no']; ?></td>
                                             <td><?= $items['Email']; ?></td>
@@ -170,8 +169,43 @@
                                     <?php
                                 }
                             }
-                            
-        
+                            //SEARCH BY SPECIFIC DATE
+                            elseif (!empty($_GET['date'])) {
+                                $employee_no = $_GET['employee_no'];
+                                $date = $_GET['date'];
+                                //SQL COMMAND TO SORT BY MM-
+                                $query = "SELECT * FROM `user_diwar_record` WHERE `Employee_no` = '$employee_no' AND `Date` = '$date' ";
+                                $query_run = mysqli_query($mysqli, $query);
+                                //IF RESULT IS > 0
+                                if(mysqli_num_rows($query_run) > 0)
+                                {
+                                    foreach($query_run as $items)
+                                    {
+                                        ?>
+                                        <tr>
+                                            <!-- $items['COLUMN NAME IN USER_DIWAR DB'] -->
+                                            <td><?= $items['Employee_no']; ?></td>
+                                            <td><?= $items['Email']; ?></td>
+                                            <td><?= $items['Name']; ?></td>
+                                            <td><?= $items['Date']; ?></td>
+                                            <td><?= $items['Time_from']; ?></td>
+                                            <td><?= $items['Time_to']; ?></td>
+                                            <td><?= $items['Output']; ?></td>
+                                            <td><?= $items['Details']; ?></td>
+                                            <td><?= $items['Verification']; ?></td>
+                                        </tr>
+                                        <?php
+                                    }
+                                }
+                                else
+                                {
+                                    ?>
+                                        <tr>
+                                            <td colspan="4">No Record Found</td>
+                                        </tr>
+                                    <?php
+                                }
+                            }
                         ?>         
                     </tbody> 
                 </table>
